12db263e532b8191814b2acae9987b665e4b17ca
Dairo Carrasquilla <dcarrasquilla@ises.com.co>
2026-03-25 09:00:05 -0500

    chore: avances 20260325.900

--- src/app/mano_obra/mano_obra_controller.ts
+++ src/app/mano_obra/mano_obra_controller.ts
@@ -1,6 +1,5 @@
-import { AllQueryDto, BrigadaQueryDto, PeriodosQueryDto, TecnicoQueryDto } from "./mano_obra_dto";
-import { Body, Controller, Get, Patch, Query } from "@nestjs/common";
 import { ManoObraService } from "./mano_obra_service";
+import { Controller, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 
 @ApiTags("Mano de obra")
@@ -8,28 +7,8 @@ import { ApiTags } from "@nestjs/swagger";
 export class ManoObraController {
   constructor (private readonly service: ManoObraService) {}
 
-  @Patch()
-  async get_mano_obra(@Body() query:AllQueryDto) {
-    return this.service.get_mano_obra(query);
-  }
-
-  @Get("proyectos")
-  async get_proyectos() {
-    return this.service.get_proyectos();
-  }
-
-  @Get("periodos")
-  async get_periodos(@Query() query:PeriodosQueryDto) {
-    return this.service.get_periodos(query);
-  }
-
-  @Get("brigadas")
-  async get_brigadas(@Query() query:BrigadaQueryDto) {
-    return this.service.get_brigadas(query);
-  }
-
-  @Get("tecnicos")
-  async get_tecnicos(@Query() query:TecnicoQueryDto) {
-    return this.service.get_tecnicos(query);
+  @Post()
+  async all() {
+    return this.service.all();
   }
 }
--- src/app/mano_obra/mano_obra_service.ts
+++ src/app/mano_obra/mano_obra_service.ts
@@ -1,4 +1,3 @@
-import { AllQueryDto, BrigadaQueryDto, PeriodosQueryDto, TecnicoQueryDto } from "./mano_obra_dto";
 import { Injectable } from "@nestjs/common";
 import { DataSource } from "typeorm";
 
@@ -6,82 +5,19 @@ import { DataSource } from "typeorm";
 export class ManoObraService {
   constructor (private readonly dt: DataSource) {}
 
-  async get_mano_obra(query:AllQueryDto) {
-    const is_cero = (items:string[]) => {
-      return items.includes('0') ? [] : items;
-    }
-
-    const proyectos = is_cero(query.proyectos);
-    const periodos = is_cero(query.periodos);
-    const brigadas = is_cero(query.brigadas);
-    const tecnicos = is_cero(query.tecnicos);
-    const actividad = [];
-
-    console.log("proyectos:", proyectos);
-    console.log("periodos:", periodos);
-    console.log("brigada:", brigadas);
-    console.log("tecnicos:", tecnicos);
-
-    // const data = await this.dt.query(`
-    //   SELECT * FROM maestro_mano_obra mmo
-    //   WHERE mmo.eliminado = $1
-    //   and (cardinality($2::varchar[]) = 0 or mmo.zona = any($2))
-    // `, [false, proyectos]);
-
-    return  [];
-  }
-
-  async get_proyectos() {
-    const data = await this.dt.query(`
-      select mmo.zona as proyecto
-      from maestro_mano_obra mmo
-      where mmo.eliminado = $1
-      group by mmo.zona order by mmo.zona asc;
-    `, [false]);
-
-    return data ?? [];
-  }
-
-  async get_periodos(query:PeriodosQueryDto) {
-    const data = await this.dt.query(`
-      select to_char(mmo.fecha, 'YYYY-MM') as periodo
-      from maestro_mano_obra mmo
-      where mmo.eliminado = false
+  async all() {
+    return this.dt.query<any[]>(`
+      SELECT nic, orden, contrata, territorio, zona, municipio, corregimiento,
+      localidad_barrio, tarifa, tipo_actividad, actividad, direccion, id_transformador,
+      id_circuito, num_medidor, marca_medidor, deuda_act, deuda_cierre, cant_factura_act,
+      cant_factura_cierre, tipo_os, descripcion_tipo_os, tipo_suspension_solicitada,
+      tipo_brigada, id_tecnico, tecnico, av_resultado, accion, subaccion_subanomalia,
+      estado_osf, estado_siprem, to_char(mmo.fecha, 'YYYY-MM') as fecha, hora
+      FROM maestro_mano_obra mmo
+      WHERE mmo.eliminado = $1
       and mmo.fecha is not null
-      and mmo.zona = any($1)
-      and mmo.eliminado = $2
-      group by periodo
-      order by periodo desc;
-    `, [query.proyectos, false]);
-
-    return data ?? [];
-  }
-
-  async get_brigadas(query:BrigadaQueryDto) {
-    const data = await this.dt.query(`
-      select mmo.tipo_brigada as brigada
-      from maestro_mano_obra mmo
-      where  mmo.zona = any($1)
-      and to_char(mmo.fecha , 'YYYY-MM') = any($2)
       and mmo.tipo_brigada is not null
-      and mmo.eliminado = $3
-      group by mmo.tipo_brigada;
-    `, [query.proyectos, query.periodos, false]);
-
-    return data ?? [];
-  }
-
-  async get_tecnicos(query:TecnicoQueryDto) {
-    const data = await this.dt.query(`
-      select mmo.tecnico
-      from maestro_mano_obra mmo
-      where  mmo.zona = any($1)
-      and to_char(mmo.fecha , 'YYYY-MM') = any($2)
-      and mmo.tipo_brigada = any($3)
-      and mmo.eliminado = $4
-      group by mmo.tecnico;
-    `, [query.proyectos, query.periodos, query.brigadas, false])
-
-    return data ?? [];
+      and mmo.tecnico is not null
+    `, [false]);
   }
 }
