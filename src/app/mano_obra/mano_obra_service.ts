c0a1fabd837b547c7ad81e8189955cb2437ec129
Dairo Carrasquilla <dcarrasquilla@ises.com.co>
2026-03-26 17:32:47 -0500

    chore: avances 20260326.1732

--- src/app/mano_obra/mano_obra_service.ts
+++ src/app/mano_obra/mano_obra_service.ts
@@ -7,17 +7,18 @@ export class ManoObraService {
 
   async all() {
     return this.dt.query<any[]>(`
-      SELECT nic, orden, contrata, territorio, zona, municipio, corregimiento,
-      localidad_barrio, tarifa, tipo_actividad, actividad, direccion, id_transformador,
-      id_circuito, num_medidor, marca_medidor, deuda_act, deuda_cierre, cant_factura_act,
-      cant_factura_cierre, tipo_os, descripcion_tipo_os, tipo_suspension_solicitada,
-      tipo_brigada, id_tecnico, tecnico, av_resultado, accion, subaccion_subanomalia,
-      estado_osf, estado_siprem, to_char(mmo.fecha, 'YYYY-MM') as fecha, hora
+      select distinct mmo.nic, mmo.orden, mmo.zona, mmo.tipo_brigada,
+      mmo.tipo_os, mmo.tecnico, to_char(mmo.fecha, 'YYYY-MM') as periodo,
+      mmo.fecha, mmo.hora::time, to_char(mmo.hora::time, 'HH24:00') as tiempo,
+      mpc.estado, mpc.valor_unitario, subaccion_subanomalia as tipo_actividad,
+      to_char(mmo.fecha, 'DD') as periodo_dia
       FROM maestro_mano_obra mmo
+      left join maestro_pagos_csr mpc on mpc.accion = mmo.accion
       WHERE mmo.eliminado = $1
       and mmo.fecha is not null
       and mmo.tipo_brigada is not null
       and mmo.tecnico is not null
+      order by mmo.fecha asc, mmo.hora::time asc;
     `, [false]);
   }
 }
